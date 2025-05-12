19cfe19daa11b4bb9fc62d860fa6d4a98f9775d8
Arseniy Ott <arseniy.ott@mail.ru>
2025-05-12 20:55:50 +0200

    Update config.py

--- Project/src/config/config.py
+++ Project/src/config/config.py
@@ -1,14 +1,21 @@
+import os
+
 from sqlalchemy.engine import URL
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port_str = os.getenv("DB_PORT")
+port = int(port_str) if port_str and port_str.isdigit() else None
+
 
 DATABASE_URL = URL.create(
-    drivername="postgresql+asyncpg",
-    username="postgres",
-    password="2191",
-    host="localhost",
-    port=5432,
-    database="schedule_medications"
+    drivername=str(os.getenv("DB_DRIVER")),
+    username=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=port,
+    database=os.getenv("DB_NAME"),
 )
-
-LOG_LVL = 'INFO'
 
 TIME_PERIOD_HOURS = 1
