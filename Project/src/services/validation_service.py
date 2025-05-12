9bc6ccd2bbd6588fbdeb348c9d2441217a53af54
Arseniy Ott <arseniy.ott@mail.ru>
2025-05-12 20:56:59 +0200

    Update validation_service.py

--- Project/src/services/validation_service.py
+++ Project/src/services/validation_service.py
@@ -5,12 +5,19 @@ def validate_id(name: str):
     async def dependency(request: Request):
         value = request.query_params.get(name)
         if value is None:
-            raise HTTPException(status_code=422, detail=f"Missing required query parameter '{name}'")
+            raise HTTPException(
+                status_code=422, detail=f"Missing required query parameter '{name}'"
+            )
         try:
             valid_number_id = int(value)
             if valid_number_id <= 0:
-                raise HTTPException(status_code=400, detail=f"{name} must be a positive integer")
+                raise HTTPException(
+                    status_code=400, detail=f"{name} must be a positive integer"
+                )
             return valid_number_id
         except ValueError:
-            raise HTTPException(status_code=400, detail=f"{name} must be a valid integer")
+            raise HTTPException(
+                status_code=400, detail=f"{name} must be a valid integer"
+            )
+
     return dependency
