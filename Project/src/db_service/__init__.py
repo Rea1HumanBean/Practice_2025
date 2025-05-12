f3daa3e590ee1527a0a49b7513a8656bf956fc5e
Arseniy Ott <arseniy.ott@mail.ru>
2025-05-12 20:55:57 +0200

    Update __init__.py

--- Project/src/db_service/__init__.py
+++ Project/src/db_service/__init__.py
@@ -1,5 +1,5 @@
-from .postgres_repository import PostgreSQLScheduleRepository
-from .db import init_db, SessionLocal
-from .medication_schedule import MedicationSchedule
-from src.db_service.db import Base
-
+from src.db_service.base_repository import BaseRepository
+from src.db_service.postgres_repository import PostgreSQLScheduleRepository
+from src.db_service.db import SessionLocal, Base
+from src.db_service.medication_schedule import MedicationSchedule
+from src.db_service.db import SessionLocal, engine, init_db, get_db
